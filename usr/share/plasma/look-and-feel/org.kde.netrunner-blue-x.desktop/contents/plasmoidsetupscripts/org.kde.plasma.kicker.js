be9f1656b5addffe947c770454885fa191e45f91
AJSlye <jameskittsmiller@gmail.com>
2016-01-04 18:53:40 -0500

    Add kicker based defaults

---
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-blue-x.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kicker.js
@@ -0,0 +1,5 @@
+applet.currentConfigGroup = ["General"]
+applet.writeConfig("customButtonImage", "file:///usr/share/pixmaps/swirl-run-4.png")
+applet.writeConfig("favoriteApps", ["systemsettings.desktop", "org.kde.ksysguard.desktop", "octopi.desktop", "vokoscreen.desktop"])
+applet.writeConfig("limitDepth", true)
+applet.writeConfig("useCustomButtonImage", true)
