1745eb44d9cb694efdd9e5f774505580cbce91a6
AJSlye <jameskittsmiller@gmail.com>
2016-02-04 07:12:43 -0500

    remove custom buttons
    Will now default to the desktop themes button.

--- usr/share/plasma/look-and-feel/org.kde.netrunner-blue-x.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kicker.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-blue-x.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kicker.js
@@ -1,5 +1,3 @@
 applet.currentConfigGroup = ["General"]
-applet.writeConfig("customButtonImage", "file:///usr/share/pixmaps/swirl-run-4.png")
 applet.writeConfig("favoriteApps", ["systemsettings.desktop", "org.kde.ksysguard.desktop", "octopi.desktop", "vokoscreen.desktop"])
 applet.writeConfig("limitDepth", true)
-applet.writeConfig("useCustomButtonImage", true)
